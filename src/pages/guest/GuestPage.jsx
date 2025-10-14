8d77ca3c5861d0a8eeec76b0055820f8b6377f1b
Hamm20-source <fananiilham20@gmail.com>
2025-10-14 14:35:49 +0700

    Edit Layout

--- src/pages/guest/GuestPage.jsx
+++ src/pages/guest/GuestPage.jsx
@@ -10,12 +10,12 @@ export default function GuestPage() {
 
   return (
     <div className='flex container mx-auto justify-center items-center h-screen '>
-      <div className='flex flex-col justify-center items-center space-y-20  bg-[#FBFBFB] shadow-lg p-20 border border-gray-200 rounded-lg m-10 md:m-0'>
-        <h1 className='text-xl font-medium'>Anda belum bisa masuk sesi ini, silahkan</h1>
+      <div className='flex flex-col justify-center items-center space-y-20  bg-[#FBFBFB] shadow-lg p-5 md:p-20 border border-gray-200 rounded-lg m-10 md:m-0'>
+        <h1 className='text-sm md:text-xl font-medium'>Anda belum bisa masuk sesi ini, silahkan</h1>
         <div>
           <button 
             onClick={loginButton} 
-            className='bg-[#59C883] px-15 py-3 text-white font-bold rounded-full text-lg cursor-pointer'
+            className='bg-[#59C883] px-5 py-2 text-white text-sm md:text-xl font-bold rounded-full text-lg cursor-pointer'
           > 
             Login 
           </button>
