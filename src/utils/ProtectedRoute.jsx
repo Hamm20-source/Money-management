7ec1d60a4625c473e4e8b90f1399a68c1aad2494
Hamm20-source <fananiilham20@gmail.com>
2025-10-14 18:28:45 +0700

    Repair

--- src/utils/ProtectedRoute.jsx
+++ src/utils/ProtectedRoute.jsx
@@ -2,7 +2,6 @@ import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute = () => {
     const Token = localStorage.getItem("accessToken")
-
     return Token ? <Outlet/> : <Navigate to="/guest" replace/>
 
 };
