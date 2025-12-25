5b2aa869ea3a16fcfdfdbe3e7eef3b78b30ea190
Hamm20-source <fananiilham20@gmail.com>
2025-12-25 20:07:02 +0700

    Adding Refresh key to adding income and expense load pages

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -2,17 +2,23 @@ import GridTransaction from '../../components/transactions/GridTransaction'
 import DoughnutChart from '../../features/charts/DoughnutChart'
 import LineChart from '../../features/charts/LineChart'
 import TransactionTable from '../../components/transactions/TransactionTable'
+import { useState } from 'react'
 
 export default function Dashboard({uid}) {
+  const [refreshKey, setRefreshKey] = useState(0);
+
+  const handleRefresh = () => {
+    setRefreshKey(prev => prev + 1);
+  }
 
   return (
     <div className='m-2 px-4 py-2 space-y-10'>
-          <GridTransaction uid={uid}/>
+          <GridTransaction uid={uid} refreshKey={refreshKey}/>
         <div className='flex flex-col md:flex-row gap-5'>
-          <DoughnutChart uid={uid}/>
-          <LineChart uid={uid}/>
+          <DoughnutChart uid={uid} refreshKey={refreshKey}/>
+          <LineChart uid={uid} refreshKey={refreshKey}/>
         </div>
-          <TransactionTable/>
+          <TransactionTable refreshKey={refreshKey} onRefresh={handleRefresh}/>
     </div>
   )
 }
